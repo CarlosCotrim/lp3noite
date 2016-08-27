5f941cbb799d2517f59a25ae7a4794210a82004f
CarlosCotrim <carloscotrim7@gmail.com>
2016-08-26 21:01:06 -0300

    teste

---
+++ TodoWeb2/Models/Tarefa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TodoWeb2.Models
+{
+    public class Tarefa
+    {
+        public int TarefaId { get; set; }
+        public string Texto { get; set; }
+        public bool Concluida { get; set; }
+
+        #region chaves estrangeiras
+        public int UsuarioId { get; set; }
+        public int CategoriaId { get; set; }
+        #endregion
+    }
+}
